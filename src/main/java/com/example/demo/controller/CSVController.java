d1ab552c16ca21f8d7c914668f4ced7cc9b912be
Renuka <renuka@interleap.co>
2022-01-31 11:04:30 +0530

    added configuration to resolve cors domain issue

--- src/main/java/com/example/demo/controller/CSVController.java
+++ src/main/java/com/example/demo/controller/CSVController.java
@@ -6,7 +6,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import com.example.demo.service.CSVService;
 
 @RestController
 @RequestMapping("/")
+
 public class CSVController {
 	
 	@Autowired
@@ -28,8 +31,8 @@ public class CSVController {
 	}
 	
 	
-	
-	@GetMapping("api/csv/upload")
+	@CrossOrigin
+	@PostMapping("api/csv/upload")
 	public ResponseEntity<?> uploadCSV(@RequestParam("file") MultipartFile file) {
 		return new ResponseEntity<>(csvService.getCSVFileContent(file),HttpStatus.OK);
 	}
